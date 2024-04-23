031b5123d9a87aea78fedddc7ba06b5a1877bc70
rafi021 <mahmud.ibrahim021@gmail.com>
2024-04-23 22:58:44 +0600

    hello world

--- Hello.java
+++ Hello.java
@@ -1,3 +1,10 @@
+/*
+ * step1: complie java code each time to see result
+ * -- $ javac Hello.java
+ * step2: then run the code
+ * -- $ java Hello
+ */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World! From Java");
