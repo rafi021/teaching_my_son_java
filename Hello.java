80fa7b02c8bff6460f975bde02f7cea3338d09b1
rafi021 <mahmud.ibrahim021@gmail.com>
2024-04-23 23:06:04 +0600

    print text and numbers

--- Hello.java
+++ Hello.java
@@ -7,8 +7,16 @@
 
 public class Hello {
     public static void main(String[] args) {
+        // Print Strings / text
         System.out.println("Hello World! From Java");
         System.out.print("This is Omar bin Ibrahim Araf");
         System.out.println("I am a new line printer");
+
+        // Print numbers
+        System.out.println(3);
+        System.out.println(30000000);
+        System.out.println(30.939081394);
+        System.out.println(30.939081394*2.3);
+        System.out.println(30*3-10);
     }
 }
